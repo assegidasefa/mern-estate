b9babfd49e27bc93a0fc374fb0c199a0a0f976fe
assegidasefa <assegidassefa7@gmail.com>
2023-12-13 13:21:55 +0300

    complete signup page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -9,73 +9,74 @@ export default function SignUp() {
   const navigate = useNavigate();
 
   const handleChange = (e) => {
-    // setFormData({
-    //   ...formData,
-    //   [e.target.id]: e.target.value,
-    // });
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
   };
 
+  console.log("form : ",formData)
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     setLoading(true);
-  //     const res = await fetch('/api/auth/signup', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify(formData),
-  //     });
-  //     const data = await res.json();
-  //     console.log(data);
-  //     if (data.success === false) {
-  //       setLoading(false);
-  //       setError(data.message);
-  //       return;
-  //     }
-  //     setLoading(false);
-  //     setError(null);
-  //     navigate('/sign-in');
-  //   } catch (error) {
-  //     setLoading(false);
-  //     setError(error.message);
-  //   }
-  // };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form 
-      // onSubmit={handleSubmit} 
+      onSubmit={handleSubmit} 
       className='flex flex-col gap-4'>
         <input
           type='text'
           placeholder='username'
           className='border p-3 rounded-lg'
           id='username'
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <input
           type='email'
           placeholder='email'
           className='border p-3 rounded-lg'
           id='email'
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <input
           type='password'
           placeholder='password'
           className='border p-3 rounded-lg'
           id='password'
-          // onChange={handleChange}
+          onChange={handleChange}
         />
 
         <button
-          // disabled={loading}
+          disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
-          {/* {loading ? 'Loading...' : 'Sign Up'} */}
-          signup
+          {loading ? 'Loading...' : 'Sign Up'} 
         </button>
         {/* <OAuth/> */}
       </form>
@@ -85,7 +86,7 @@ export default function SignUp() {
           <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
-      {/* {error && <p className='text-red-500 mt-5'>{error}</p>} */}
+       {error && <p className='text-red-500 mt-5'>{error}</p>} 
     </div>
   );
 }
