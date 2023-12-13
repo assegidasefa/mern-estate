5eff4d0ec172ff91a994071774e58c5ae8111d2b
assegidasefa <assegidassefa7@gmail.com>
2023-12-13 12:19:32 +0300

    create middlerware and a function to handle possible error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -12,8 +12,8 @@ export const signup = async (req, res, next) => {
     await newUser.save();
     res.status(201).json('User created successfully!');
   } catch (error) {
-    // next(error);
-    res.status(500).json(error.message)
+    next(error);
+    // res.status(500).json(error.message)
   }
 };
 
--- api/index.js
+++ api/index.js
@@ -45,12 +45,12 @@ app.use('/api/auth', authRouter);
 //   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 // })
 
-// app.use((err, req, res, next) => {
-//   const statusCode = err.statusCode || 500;
-//   const message = err.message || 'Internal Server Error';
-//   return res.status(statusCode).json({
-//     success: false,
-//     statusCode,
-//     message,
-//   });
-// });
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
