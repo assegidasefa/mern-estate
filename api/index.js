3f4ae8aafbf6ee33f1b9859cd67cbfec731b5387
assegidasefa <assegidassefa7@gmail.com>
2023-12-13 12:08:27 +0300

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
-// import authRouter from './routes/auth.route.js';
+import authRouter from './routes/auth.route.js';
 // import listingRouter from './routes/listing.route.js';
 // import cookieParser from 'cookie-parser';
 // import path from 'path';
@@ -22,7 +22,7 @@ mongoose
 
 const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 
 // app.use(cookieParser());
 
@@ -35,7 +35,7 @@ app.listen(3000, () => {
 //   res.send("hello world")
 // })
 app.use('/api/user', userRouter);
-// app.use('/api/auth', authRouter);
+app.use('/api/auth', authRouter);
 // app.use('/api/listing', listingRouter);
 
 
