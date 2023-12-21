c7e431258379126acfbca93ca3b2aed0d6364a5b
assegidasefa <assegidassefa7@gmail.com>
2023-12-21 23:50:44 +0300

    add signup functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,7 +14,9 @@ import {
   deleteUserFailure,
   deleteUserSuccess,
   deleteUserStart,
-  // signOutUserStart,
+  signOutUserStart,
+  signOutUserSuccess,
+  signOutUserFailure,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -117,12 +119,18 @@ export default function Profile() {
       const res = await fetch('/api/auth/signout');
       const data = await res.json();
       if (data.success === false) {
-        dispatch(deleteUserFailure(data.message));
+        // dispatch(deleteUserFailure(data.message));
+        dispatch(signOutUserFailure(data.message));
+
         return;
       }
-      dispatch(deleteUserSuccess(data));
+      // dispatch(deleteUserSuccess(data));
+      dispatch(signOutUserSuccess(data));
+      
     } catch (error) {
-      dispatch(deleteUserFailure(data.message));
+      // dispatch(deleteUserFailure(data.message));
+      dispatch(signOutUserFailure(data.message));
+
     }
   };
 
